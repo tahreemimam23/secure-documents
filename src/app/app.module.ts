68f298010bb3f7cda3d6e78fefabd321e979b1ee
tahreemimam23 <imamtahreem23@gmail.com>
2024-10-12 17:59:25 +0530

    basic setup

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { importProvidersFrom, NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule, RouterOutlet } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { routes } from "./app.routes";
+import { firebaseConfig } from "./constants/firebase-config";
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { AngularFireDatabaseModule } from "@angular/fire/compat/database";
+import { AngularFirestoreModule } from "@angular/fire/compat/firestore";
+
+@NgModule({
+    declarations: [],
+    bootstrap: [AppComponent],
+    imports: [
+        RouterOutlet,
+        BrowserModule,
+        CommonModule,
+        RouterModule.forRoot(routes)
+    ],
+    providers: [
+        importProvidersFrom([
+            AngularFireModule.initializeApp(firebaseConfig),
+            AngularFireAuthModule,
+            AngularFireDatabaseModule,
+            AngularFirestoreModule
+        ])
+    ]
+})
+export class AppModule {
+
+}
